afec9347d9580ba0707ffbca1b35fcc01bbc8056
kennedy <kitongakennedy9@gmail.com>
2024-03-27 09:25:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = 5
+Math.floor(Math.random() * 10 +3)
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+console.log(mod)
+
+let max = Math.max(0, 8 ,12, 20)
+console.log(max)
